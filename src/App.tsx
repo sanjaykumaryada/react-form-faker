285e32e4c08ed55cd0c73f3f0e933823b923e4d4
sanjay <sanjay@mithyalabs.com>
2022-11-08 16:25:09 +0530

    handled edge cases

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,42 @@
-import React,{useState} from "react";
-import {formConfig, getFakerData} from "./fakerData";
+import React, { useState } from "react";
+import { formConfig, getFakerData } from "./fakerData";
 import { IFormInput } from "./model";
 
 function App() {
-  const [inputData, setInputData]=useState<IFormInput>({
-    firstName:"",
-    lastName:"",
-    middleName:"",
-    email:"",
-    phoneNumber:"",
-    website:"",
-    password:"",
-    rePassword:"",
-    name:"",
-    age:0,
-    gender:"",
-    checkbox:"",
+  const [inputData, setInputData] = useState<IFormInput>({
+    firstName: "",
+    lastName: "",
+    middleName: "",
+    email: "",
+    phoneNumber: "",
+    website: "",
+    password: "",
+    rePassword: "",
+    name: "",
+    age: 0,
+    gender: "",
+    checkbox: "",
   })
-  const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
-    setInputData({...inputData,[e.target.name]:e.target.value})
-}
-  const handleAutofill=(e:React.FormEvent)=>{
-    e.preventDefault();
-    const fakerData=getFakerData(formConfig);
-    console.log(fakerData);
-    setInputData({...inputData,
-      firstName:fakerData.firstName,
-      lastName:fakerData.lastName,
-      email:fakerData.email,
-      website:fakerData.website,
-      password:fakerData.password,
-      phoneNumber:fakerData.phoneNumber,
-      rePassword:fakerData.password
-    })
-  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value })
   }
- const handleRegister=(e:React.FormEvent)=>{
-  e.preventDefault();
- }
+  const handleAutofill = (e: React.FormEvent) => {
+    e.preventDefault();
+    const fakerData = getFakerData(formConfig);
+    console.log(fakerData);
+    setInputData({
+      ...inputData,
+      firstName: fakerData.firstName,
+      lastName: fakerData.lastName,
+      email: fakerData.email,
+      website: fakerData.website,
+      password: fakerData.password,
+      phoneNumber: fakerData.phoneNumber,
+      rePassword: fakerData.password
+    })
+
+  }
+
   return (
     <div className="bg-blue-50 pl-60">
       <div className="pl-60 py-40 h-screen">
@@ -52,81 +51,81 @@ function App() {
             <div className="flex flex-col pr-10">
               <label >First Name</label>
               <input
-               className="h-10 px-4 rounded-sm mt-2 shadow-md" 
-               placeholder="e.g. Sanjay" 
-               name="firstName"
-               type="text"
-               value={inputData.firstName}
-               onChange={handleChange}
-               />
+                className="h-10 px-4 rounded-sm mt-2 shadow-md"
+                placeholder="e.g. Sanjay"
+                name="firstName"
+                type="text"
+                value={inputData.firstName}
+                onChange={handleChange}
+              />
             </div>
             <div className="flex flex-col">
               <label>Last Name</label>
-              <input 
-              className="h-10 px-4 rounded-sm mt-2 shadow-md" 
-              placeholder="e.g. Kumar" 
-              name="lastName"
-              type="text"
-              value={inputData.lastName}
-              onChange={handleChange}
+              <input
+                className="h-10 px-4 rounded-sm mt-2 shadow-md"
+                placeholder="e.g. Kumar"
+                name="lastName"
+                type="text"
+                value={inputData.lastName}
+                onChange={handleChange}
               />
             </div>
           </div>
           <div className="flex flex-col">
             <label>Email Address</label>
-            <input 
-            className="h-10 w-[470px] px-4 rounded-sm mt-2 shadow-md"
-             placeholder="e.g. 2408sanjay@gmail.com" 
-             name="email"
-             type="email"
-             value={inputData.email}
-             onChange={handleChange}
-             />
+            <input
+              className="h-10 w-[470px] px-4 rounded-sm mt-2 shadow-md"
+              placeholder="e.g. 2408sanjay@gmail.com"
+              name="email"
+              type="email"
+              value={inputData.email}
+              onChange={handleChange}
+            />
           </div>
           <div className="flex mt-4 mb-6">
             <div className="flex flex-col pr-10">
               <label>Phone Number</label>
               <input
-               className="h-10 px-4 rounded-sm mt-2 shadow-md" 
-               placeholder="+91-99999-99999"
-               type="tel"
-               name="phoneNumber"
-               value={inputData.phoneNumber}
-               onChange={handleChange}
-               />
+                className="h-10 px-4 rounded-sm mt-2 shadow-md"
+                placeholder="+91-99999-99999"
+                type="tel"
+                name="phoneNumber"
+                value={inputData.phoneNumber}
+                onChange={handleChange}
+              />
             </div>
             <div className="flex flex-col">
               <label>Website</label>
               <input
-               className="h-10 px-4 rounded-sm mt-2 shadow-md"
-                placeholder="https://www.google.com" 
+                className="h-10 px-4 rounded-sm mt-2 shadow-md"
+                placeholder="https://www.google.com"
                 type="text"
                 name="website"
                 value={inputData.website}
-                onChange={handleChange}/>
+                onChange={handleChange} />
             </div>
           </div>
           <div className="flex mt-4 mb-6">
             <div className="flex flex-col pr-10">
               <label>Your Password</label>
-              <input 
-              className="h-10 px-4 rounded-sm mt-2 shadow-md" 
-              placeholder="******"
-              type="password" 
-              name="password"
-              value={inputData.password}
-              onChange={handleChange}
+              <input
+                className="h-10 px-4 rounded-sm mt-2 shadow-md"
+                placeholder="******"
+                type="password"
+                name="password"
+                value={inputData.password}
+                onChange={handleChange}
               />
             </div>
             <div className="flex flex-col">
               <label>Re-type Password</label>
-              <input 
-              className="h-10 px-4 rounded-sm mt-2 shadow-md" 
-              type="password"
-              placeholder="******" 
-              name="rePassword"
-              value={inputData.rePassword}
-              onChange={handleChange}
+              <input
+                className="h-10 px-4 rounded-sm mt-2 shadow-md"
+                type="password"
+                placeholder="******"
+                name="rePassword"
+                value={inputData.rePassword}
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -134,7 +133,6 @@ function App() {
         <div className="flex">
           <div className=" flex flex-col">
             <button className="bg-orange-500 rounded-md mb-4 h-11 p-3 text-white" onClick={handleAutofill} >AutoFill</button>
-            <button className="bg-orange-500 rounded-md text-white h-11" onClick={handleRegister}>Register</button>
           </div>
         </div>
       </div>
--- src/fakerData.tsx
+++ src/fakerData.tsx
@@ -1,86 +1,73 @@
 import { faker } from '@faker-js/faker';
-import { IFormConfig} from './model';
+import { IFormConfig } from './model';
 export const formConfig: IFormConfig[] = [
     {
         type: "name",
-        valueKey:"name",
+        valueKey: "name",
     },
     {
         type: "firstName",
-        valueKey:'firstName'
+        valueKey: 'firstName'
     },
     {
         type: "middleName",
-        valueKey:"middleName",
+        valueKey: "middleName",
     },
     {
         type: "lastName",
-        valueKey:'lastName'
+        valueKey: 'lastName'
     },
     {
         type: "age",
-        valueKey:"age"
+        valueKey: "age"
 
     },
     {
         type: "email",
-        valueKey:"email",
+        valueKey: "email",
     },
     {
-        type: "phone",
-        valueKey:'phone',
+        type: "phoneNumber",
+        valueKey: 'phoneNumber',
     },
     {
         type: "password",
-        valueKey:"password",
+        valueKey: "password",
     },
     {
-        type: "url",
-        valueKey:"url",
+        type: "website",
+        valueKey: "website",
 
     }, {
         type: "birthdate",
-        valueKey:"birthdate",
+        valueKey: "birthdate",
     },
     {
         type: 'gender',
-        valueKey:"gender",
-        options: ['male', "female"]
+        valueKey: "gender",
+        options: ["male", "female"]
     },
     {
         type: 'checkbox',
-        valueKey:"checkbox",
+        valueKey: "checkbox",
         options: ['study', 'cricket', 'football', "coding"]
     }
 ]
 export const getFakerData = (formConfig: IFormConfig[]) => {
 
-    const values:{[key:string]:string} = {
-        firstName: '',
-        lastName: '',
-        middleName: "",
-        email: '',
-        phoneNumber: '',
-        website: '',
-        password: '',
-        rePassword: '',
-        name: "",
-        age: "",
-        gender: "",
-        checkbox: "",
-    }
+    const values: { [key: string]: string } = {}
 
 
     formConfig.forEach((config: IFormConfig) => {
         switch (config.type) {
             case "firstName":
-                values[config.valueKey]= faker.name.firstName();
+                values[config.valueKey] = faker.name.firstName();
                 break;
             case 'lastName':
                 values[config.valueKey] = faker.name.lastName();
                 break;
             case 'name':
-                values[config.valueKey]= faker.name.fullName();
+                values[config.valueKey] = faker.name.fullName();
                 break;
             case 'middleName':
                 values[config.valueKey] = faker.name.middleName();
@@ -89,25 +76,33 @@ export const getFakerData = (formConfig: IFormConfig[]) => {
                 values[config.valueKey] = faker.internet.email();
                 break;
             case 'age':
-                values[config.valueKey] = (Math.floor(Math.random() * 100)+10).toString();
+                values[config.valueKey] = (Math.floor(Math.random() * 100) + 10).toString();
                 break;
-            case 'url':
+            case 'website':
                 values[config.valueKey] = faker.internet.url();
                 break;
             case 'password':
                 values[config.valueKey] = faker.internet.password();
                 break;
-            case 'phone':
+            case 'phoneNumber':
                 values[config.valueKey] = faker.phone.number('+91-#####-#####');
                 break;
             case 'gender':
-                    if (config.options !== undefined){
-                    values[config.valueKey]= config.options[Math.floor(Math.random() * config.options.length)]
-                    }
+                if (config.options !== undefined && config.options.length !== 0){
+                    values[config.valueKey] = config.options[Math.floor(Math.random() * config.options.length)]
+                }else{
+                    throw new Error("Please fill atleast one option");
+                    
+                }
                 break;
             case 'checkbox':
-                    if (config.options !== undefined)
-                    values[config.valueKey]= config.options[Math.floor(Math.random() * config.options.length)]
+                if (config.options !== undefined && config.options.length !== 0){
+                    values[config.valueKey] = config.options[Math.floor(Math.random() * config.options.length)];
+                }else{
+                        throw new Error("Please fill atleast one option");
+                        
+                }
+                
                 break;
             default:
                 break;
